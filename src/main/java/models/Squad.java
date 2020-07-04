b7e378f7e4b8f014eac9fdac77cb5999af3ae63e
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-04 23:06:43 +0300

    Add a constructor to squad class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -5,4 +5,11 @@ public class Squad {
     private String name;
     private String causeToFight;
     private int id;
+
+    public Squad(int maximumSize, String name, String causeToFight) {
+        this.maximumSize = maximumSize;
+        this.name = name;
+        this.causeToFight = causeToFight;
+    }
+
 }
