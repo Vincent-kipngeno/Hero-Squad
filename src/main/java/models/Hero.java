d62dc2ec56d02cc36c506a8273cea1f09618e17a
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-04 22:02:41 +0300

    Add name,age,specialPower and weakness properties to hero class

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+    private int id;
+    private int squadId;
+
+
 }
