cdab80f6b7668367ce3dc5b65ab9098fa5c56fd8
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-05 14:19:25 +0300

    Add methods to SquadDao interface

--- src/main/java/dao/SquadDao.java
+++ src/main/java/dao/SquadDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Hero;
+import models.Squad;
+
+import java.util.List;
+
 public interface SquadDao {
+    //LIST
+    List<Squad> getAll();
+
+    //CREATE
+    void add (Squad squad);
+
+    //READ
+    Squad findById(int id);
+    List<Hero> getAllHeroesBySquad(int categoryId);
+
+    //UPDATE
+    void update(int id, String name);
+
+    //DELETE
+    void deleteById(int id);
+    void clearAllSquads();
 }
