6ceb465f91a17f8724ba87bcd3ca912521a387fd
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-05 15:35:50 +0300

    Remove age column in squads table in creqte.sql

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE hero_squad;
 \c hero_squad;
 CREATE TABLE heroes (id SERIAL PRIMARY KEY, name VARCHAR, specialPower VARCHAR, weakness VARCHAR, age INTEGER, squadId INTEGER);
-CREATE TABLE squads (id SERIAL PRIMARY KEY, maximumSize INTEGER, name VARCHAR, causeToFight VARCHAR, age INTEGER);
+CREATE TABLE squads (id SERIAL PRIMARY KEY, maximumSize INTEGER, name VARCHAR, causeToFight VARCHAR);
 CREATE DATABASE hero_squad_test WITH TEMPLATE hero_squad;
